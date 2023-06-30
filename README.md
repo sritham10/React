9af0ba9014f62b2bf474ced0896b21345ad0fe4c
sritham10 <sritham15@gmail.com>
2023-06-30 15:32:37 +0530

    React

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-#Namste REact
+#Namste React
+
+
+# parcel
+-DEv Build
+-local server
+-HMR=Hot module Replacement
+-File Watching Algorithm
+-Caching-Faster Builds
+-Image optimisation
+-minification
+-bundling
+-compress
+-DIffernt Bundling-support older broswer
+-HTTPs
+-Tree Shaking-remove unused codes 
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "I am IRONMAN"),
+    React.createElement("h1", {}, "I am IRONMN")
+]), React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {}, "I am IRONMAN"),
+    React.createElement("h1", {}, "I am IRONMN")
+]));
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
